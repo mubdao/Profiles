780b466b3e76a8ba876bf8cc46085a589f1df463
mubdao <131733693+mubdao@users.noreply.github.com>
2023-04-25 13:11:13 +0800

    Create netisp.js

---
+++ javascript/netisp.js
@@ -0,0 +1,21 @@
+let url = "http://ip-api.com/json/?lang=zh-CN"
+$httpClient.get(url, function(error, response, data){
+let jsonData = JSON.parse(data)
+let ip = jsonData.query
+let isp = jsonData.isp
+.replace(/, Inc./g, "");
+
+let country = jsonData.country;
+let city = jsonData.city;
+let citys = jsonData.regionName;
+//去重
+let locationsArray = [country, city, citys];
+let uniqueLocationsArray = [...new Set(locationsArray)];
+let uniqueLocations = uniqueLocationsArray.join("  ");
+
+  body = {
+    title: "𝗜𝗻𝘁𝗲𝗿𝗻𝗲𝘁 𝗦𝗲𝗿𝘃𝗶𝗰𝗲 𝗣𝗿𝗼𝘃𝗶𝗱𝗲𝗿",
+		//${emoji}
+    content: `${ip}  ${isp} \n${uniqueLocations}`
+   // icon: "key.icloud", 'icon-color': "#FF5A9AF9"
+  },$done(body);})
