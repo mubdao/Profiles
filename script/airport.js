b2ea6436d0c4c0371582396beac8af6ba6ce3fb0
mubdao <131733693+mubdao@users.noreply.github.com>
2023-05-15 01:39:12 +0800

    更新 airport.js

--- script/airport.js
+++ script/airport.js
@@ -8,14 +8,14 @@ let args = getArgs();
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`用量：${bytesToSize(used)} | ${bytesToSize(total)}`];
+  let content = [`UDT：${bytesToSize(used)} | ${bytesToSize(total)}`];
 
   if (resetDayLeft) {
-    content.push(`重置：剩余${resetDayLeft}天`);
+    content.push(`RST：After ${resetDayLeft} days`);
   }
   if (expire && expire !== "false") {
     if (/^[\d.]+$/.test(expire)) expire *= 1000;
-    content.push(`到期：${formatTime(expire)}`);
+    content.push(`EXP：${formatTime(expire)}`);
   }
 
   let now = new Date();
