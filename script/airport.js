4b1ab220234f5bb26bb785efe3f07b45049f4ae0
mubdao <131733693+mubdao@users.noreply.github.com>
2023-05-14 14:21:59 +0800

    更新 airport.js

--- script/airport.js
+++ script/airport.js
@@ -27,8 +27,6 @@ let args = getArgs();
   $done({
     title: `${args.title} | ${hour}:${minutes}`,
     content: content.join("\n"),
-    icon: args.icon || "airplane.circle",
-    "icon-color": args.color || "#007aff",
   });
 })();
 
