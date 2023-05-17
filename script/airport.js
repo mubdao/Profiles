127d3799e47c0fa98a0b98abf875196754cac70b
mubdao <131733693+mubdao@users.noreply.github.com>
2023-05-17 21:46:21 +0800

    Update airport.js

--- script/airport.js
+++ script/airport.js
@@ -1,6 +1,5 @@
-let args = getArgs();
-
 (async () => {
+  let args = getArgs();
   let info = await getDataInfo(args.url);
   if (!info) $done();
   let resetDayLeft = getRmainingDays(parseInt(args["reset_day"]));
@@ -8,14 +7,18 @@ let args = getArgs();
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`用量：${bytesToSize(used)} | ${bytesToSize(total)}`];
+  let content = [`已用：${toPercent(used, total)} \t|  剩余：${toMultiply(total, used)}`];
 
-  if (resetDayLeft) {
-    content.push(`重置：剩余${resetDayLeft}天`);
-  }
-  if (expire && expire !== "false") {
-    if (/^[\d.]+$/.test(expire)) expire *= 1000;
-    content.push(`到期：${formatTime(expire)}`);
+  if (resetDayLeft || expire) {
+    if (resetDayLeft && expire && expire !== "false") {
+      if (/^[\d.]+$/.test(expire)) expire *= 1000;
+      content.push(`重置：${resetDayLeft}天 \t|  ${formatTime(expire)}`);
+    } else if (resetDayLeft && !expire) {
+      content.push(`重置：${resetDayLeft}天`);
+    } else if (!resetDayLeft && expire) {
+      if (/^[\d.]+$/.test(expire)) expire *= 1000;
+      content.push(`到期：${formatTime(expire)}`);
+    }
   }
 
   let now = new Date();
@@ -25,8 +28,10 @@ let args = getArgs();
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `${args.title} | ${hour}:${minutes}`,
+    title: `${args.title} | ${bytesToSize(total)} | ${hour}:${minutes}`,
     content: content.join("\n"),
+    icon: args.icon || "airplane.circle",
+    "icon-color": args.color || "#007aff",
   });
 })();
 
@@ -40,10 +45,9 @@ function getArgs() {
 }
 
 function getUserInfo(url) {
-  let method = args.method || "head";
   let request = { headers: { "User-Agent": "Quantumult%20X" }, url };
   return new Promise((resolve, reject) =>
-    $httpClient[method](request, (err, resp) => {
+    $httpClient.get(request, (err, resp) => {
       if (err != null) {
         reject(err);
         return;
@@ -52,9 +56,7 @@ function getUserInfo(url) {
         reject(resp.status);
         return;
       }
-      let header = Object.keys(resp.headers).find(
-        (key) => key.toLowerCase() === "subscription-userinfo"
-      );
+      let header = Object.keys(resp.headers).find((key) => key.toLowerCase() === "subscription-userinfo");
       if (header) {
         resolve(resp.headers[header]);
         return;
@@ -75,7 +77,7 @@ async function getDataInfo(url) {
 
   return Object.fromEntries(
     data
-      .match(/\w+=[\d.eE+-]+/g)
+      .match(/\w+=[\d.eE+]+/g)
       .map((item) => item.split("="))
       .map(([k, v]) => [k, Number(v)])
   );
@@ -107,10 +109,40 @@ function bytesToSize(bytes) {
   return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
 }
 
+function bytesToSizeNumber(bytes) {
+  if (bytes === 0) return "0";
+  let k = 1024;
+  let i = Math.floor(Math.log(bytes) / Math.log(k));
+  return (bytes / Math.pow(k, i)).toFixed(2);
+}
+
+function toPercent(num, total) {
+  return (Math.round((num / total) * 10000) / 100).toFixed(1) + "%";
+}
+
+
+function toMultiply(total, num) {
+  let totalDecimalLen, numDecimalLen, maxLen, multiple;
+  try {
+    totalDecimalLen = total.toString().split(".").length;
+  } catch (e) {
+    totalDecimalLen = 0;
+  }
+  try {
+    numDecimalLen = num.toString().split(".").length;
+  } catch (e) {
+    numDecimalLen = 0;
+  }
+  maxLen = Math.max(totalDecimalLen, numDecimalLen);
+  multiple = Math.pow(10, maxLen);
+  const numberSize = ((total * multiple - num * multiple) / multiple).toFixed(maxLen);
+  return bytesToSize(numberSize);
+}
+
 function formatTime(time) {
   let dateObj = new Date(time);
   let year = dateObj.getFullYear();
   let month = dateObj.getMonth() + 1;
   let day = dateObj.getDate();
-  return year + "-" + month + "-" + day + "";
+  return "到期：" + year + "." + month + "." + day + " ";
 }
