613a8f58e27e94e6617667652ba1ffef96168bc4
mubdao <131733693+mubdao@users.noreply.github.com>
2023-05-23 20:44:45 +0800

    Update vscopro.js

--- script/vscopro.js
+++ script/vscopro.js
@@ -6,11 +6,11 @@ hostname = api.revenuecat.com
 
 Qx:
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/vsco.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/mubdao/Profiles/main/script/vscopro.js
 
 Sg:
 [Script]
-vsco = type=http-response,pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/chxm1023/Rewrite/main/vsco.js
+vsco = type=http-response,pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/mubdao/Profiles/main/script/vscopro.js
 */
 
 
