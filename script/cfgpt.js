e3ec6d4756d869e3c7a3bd07ed8fe39b651f1f29
mubdao <131733693+mubdao@users.noreply.github.com>
2023-05-27 18:07:00 +0800

    更新 cfgpt.js

--- script/cfgpt.js
+++ script/cfgpt.js
@@ -1,8 +1,6 @@
 let url = "http://chat.openai.com/cdn-cgi/trace";
-let tf = ["T1","XX","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BS","BD","BB","BE","BZ","BJ","BT","BA","BW","BR","BG","BF","CV","CA","CL","CO","KM","CR","HR","CY","DK","DJ","DM","DO","EC","SV","EE","FJ","FI","FR","GA","GM","GE","DE","GH","GR","GD","GT","GN","GW","GY","HT","HN","HU","IS","IN","ID","IQ","IE","IL","IT","JM","JP","JO","KZ","KE","KI","KW","KG","LV","LB","LS","LR","LI","LT","LU","MG","MW","MY","MV","ML","MT","MH","MR","MU","MX","MC","MN","ME","MA","MZ","MM","NA","NR","NP","NL","NZ","NI","NE","NG","MK","NO","OM","PK","PW","PA","PG","PE","PH","PL","PT","QA","RO","RW","KN","LC","VC","WS","SM","ST","SN","RS","SC","SL","SG","SK","SI","SB","ZA","ES","LK","SR","SE","CH","TH","TG","TO","TT","TN","TR","TV","UG","AE","US","UY","VU","ZM","BO","BN","CG","CZ","VA","FM","MD","PS","KR","TW","TZ","TL","GB"];
-let tff = ["plus","on"];
+tf=["T1","XX","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BS","BD","BB","BE","BZ","BJ","BT","BA","BW","BR","BG","BF","CV","CA","CL","CO","KM","CR","HR","CY","DK","DJ","DM","DO","EC","SV","EE","FJ","FI","FR","GA","GM","GE","DE","GH","GR","GD","GT","GN","GW","GY","HT","HN","HU","IS","IN","ID","IQ","IE","IL","IT","JM","JP","JO","KZ","KE","KI","KW","KG","LV","LB","LS","LR","LI","LT","LU","MG","MW","MY","MV","ML","MT","MH","MR","MU","MX","MC","MN","ME","MA","MZ","MM","NA","NR","NP","NL","NZ","NI","NE","NG","MK","NO","OM","PK","PW","PA","PG","PE","PH","PL","PT","QA","RO","RW","KN","LC","VC","WS","SM","ST","SN","RS","SC","SL","SG","SK","SI","SB","ZA","ES","LK","SR","SE","CH","TH","TG","TO","TT","TN","TR","TV","UG","AE","US","UY","VU","ZM","BO","BN","CG","CZ","VA","FM","MD","PS","KR","TW","TZ","TL","GB"];
 let gpt;
-let cf;
 let args = {};
 if (typeof $argument !== 'undefined') {
   $argument.split('&').forEach(item => {
@@ -10,40 +8,27 @@ if (typeof $argument !== 'undefined') {
     args[key] = value;
   });
 }
-
-$httpClient.get(url, function(error, response, data) {
+let iconUrl = args.icon || '';
+let iconColor = args['icon-color'] || '';
+$httpClient.get(url, function(error, response, data){
   let lines = data.split("\n"); 
   let cf = lines.reduce((acc, line) => {
     let [key, value] = line.split("=");
     acc[key] = value;
     return acc;
-  }, {});
-
+  },{});
   let ip = cf.ip;
-  let warp = cf.warp;
   let loc = cf.loc;
-
-  // GPT
+  //loc
   let l = tf.indexOf(loc);
   if (l != -1) {
-    gpt = "✔";
+    gpt = "GPT: Y";
   } else {
-    gpt = "❌";
+    gpt = "GPT: N";
   }
-
-  // CF
-  let w = tff.indexOf(warp);
-  if (w != -1) {
-    cf = "✔";
-  } else {
-    cf = "❌";
-  }
-
-  let content = `GPT: ${gpt}   CF: ${cf}   Loc: ${loc}`;
-
-  let body = {
-    title: "𝗖𝗵𝗮𝘁 𝗚𝗣𝗧",
-    content: content
+  body = {
+    title: "ChatGPT",
+    content: `${gpt}   Region: ${loc}  `
   };
   $done(body);
 });
