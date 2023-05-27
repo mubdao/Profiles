10089089e5dde869f28441a1b132be0914149981
mubdao <131733693+mubdao@users.noreply.github.com>
2023-05-27 18:25:13 +0800

    更新 cfgpt.js

--- script/cfgpt.js
+++ script/cfgpt.js
@@ -1,34 +1,59 @@
 let url = "http://chat.openai.com/cdn-cgi/trace";
-tf=["T1","XX","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BS","BD","BB","BE","BZ","BJ","BT","BA","BW","BR","BG","BF","CV","CA","CL","CO","KM","CR","HR","CY","DK","DJ","DM","DO","EC","SV","EE","FJ","FI","FR","GA","GM","GE","DE","GH","GR","GD","GT","GN","GW","GY","HT","HN","HU","IS","IN","ID","IQ","IE","IL","IT","JM","JP","JO","KZ","KE","KI","KW","KG","LV","LB","LS","LR","LI","LT","LU","MG","MW","MY","MV","ML","MT","MH","MR","MU","MX","MC","MN","ME","MA","MZ","MM","NA","NR","NP","NL","NZ","NI","NE","NG","MK","NO","OM","PK","PW","PA","PG","PE","PH","PL","PT","QA","RO","RW","KN","LC","VC","WS","SM","ST","SN","RS","SC","SL","SG","SK","SI","SB","ZA","ES","LK","SR","SE","CH","TH","TG","TO","TT","TN","TR","TV","UG","AE","US","UY","VU","ZM","BO","BN","CG","CZ","VA","FM","MD","PS","KR","TW","TZ","TL","GB"];
-let gpt;
-let args = {};
+let tf=["T1","XX","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BS","BD","BB","BE","BZ","BJ","BT","BA","BW","BR","BG","BF","CV","CA","CL","CO","KM","CR","HR","CY","DK","DJ","DM","DO","EC","SV","EE","FJ","FI","FR","GA","GM","GE","DE","GH","GR","GD","GT","GN","GW","GY","HT","HN","HU","IS","IN","ID","IQ","IE","IL","IT","JM","JP","JO","KZ","KE","KI","KW","KG","LV","LB","LS","LR","LI","LT","LU","MG","MW","MY","MV","ML","MT","MH","MR","MU","MX","MC","MN","ME","MA","MZ","MM","NA","NR","NP","NL","NZ","NI","NE","NG","MK","NO","OM","PK","PW","PA","PG","PE","PH","PL","PT","QA","RO","RW","KN","LC","VC","WS","SM","ST","SN","RS","SC","SL","SG","SK","SI","SB","ZA","ES","LK","SR","SE","CH","TH","TG","TO","TT","TN","TR","TV","UG","AE","US","UY","VU","ZM","BO","BN","CG","CZ","VA","FM","MD","PS","KR","TW","TZ","TL","GB"];
+let tff=["plus","on"];
+
+// 处理 argument 参数
+let titlediy;
 if (typeof $argument !== 'undefined') {
-  $argument.split('&').forEach(item => {
-    const [key, value] = item.split('=');
-    args[key] = value;
-  });
+  const args = $argument.split('&');
+  for (let i = 0; i < args.length; i++) {
+    const [key, value] = args[i].split('=');
+    if (key === 'title') {
+      titlediy = value;
+    }
+  }
 }
-let iconUrl = args.icon || '';
-let iconColor = args['icon-color'] || '';
+
+// 发送 HTTP 请求获取所在地信息
 $httpClient.get(url, function(error, response, data){
-  let lines = data.split("\n"); 
+  if (error) {
+    console.error(error);
+    $done();
+    return;
+  }
+
+  let lines = data.split("\n");
   let cf = lines.reduce((acc, line) => {
     let [key, value] = line.split("=");
     acc[key] = value;
     return acc;
-  },{});
-  let ip = cf.ip;
+  }, {});
   let loc = cf.loc;
-  //loc
-  let l = tf.indexOf(loc);
-  if (l != -1) {
-    gpt = "GPT: Y";
+
+  // 判断 ChatGPT 是否支持该国家/地区
+  let l = tf.indexOf(cf.loc);
+  let gpt;
+  if (l !== -1) {
+    gpt = "GPT: Yes";
   } else {
-    gpt = "GPT: N";
+    gpt = "GPT: No";
   }
-  body = {
-    title: "ChatGPT",
-    content: `${gpt}   Region: ${loc}  `
+
+  // 获取 Warp 状态
+  let w = tff.indexOf(cf.warp);
+  let warps;
+  if (w !== -1) {
+    warps = "CF";
+  } else {
+    warps = "Non-CF";
+  }
+
+  // 组装通知数据
+  let body = {
+    title: titlediy ? titlediy : 'ChatGPT',
+    content: `${gpt}   区域: ${loc}   ${warps}`
   };
+
+  // 发送通知
   $done(body);
 });
