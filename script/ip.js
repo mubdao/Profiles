3be07fdd4b49a61fc48a10b2851d25d2970b2b4d
mubdao <131733693+mubdao@users.noreply.github.com>
2023-06-11 02:19:05 +0800

    更新 ip.js

--- script/ip.js
+++ script/ip.js
@@ -1,13 +1,98 @@
-if ($response.statusCode != 200) {
-  $done(Null);
+const timeoutGPT = 500;
+const timeoutAPI = 800;
+let cskey = 1, gkey = 1, body = {};
+
+function getAPI(max = 1) {
+  let url = "http://ip-api.com/json/?lang=zh-CN";
+  const outnew = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error("timeout"));
+    }, timeoutAPI);
+  });
+  const renew = new Promise((resolve, reject) => {
+    $httpClient.get(url, function (error, response, data) {
+      if (error) {
+        reject(error);
+      } else {
+        let key = JSON.parse(data);
+        resolve(key);
+      }
+    });
+  });
+  return Promise.race([outnew, renew])
+    .then((key) => {
+      return key;
+    })
+    .catch((error) => {
+      if (max > 0) {
+        cskey++;
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            getAPI(max - 1).then(resolve).catch(reject);
+          }, 0);
+        });
+      } else {
+        throw error;
+      }
+    });
 }
 
-var body = $response.body;
-var obj = JSON.parse(body);
-var title = obj['country'];
-var subtitle = obj['city'] + ' ' + obj['isp'];
-var ip = obj['query'];
-var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
+function getGPT(cs = 1) {
+  let url = "http://chat.openai.com/cdn-cgi/trace";
+  const outnew = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error("timeout"));
+    }, timeoutGPT);
+  });
+  const renew = new Promise((resolve, reject) => {
+    $httpClient.get(url, function (error, response, data) {
+      if (error) {
+        reject(error);
+      } else {
+        let lines = data.split("\n");
+        let key = lines.reduce((acc, line) => {
+          let [key, value] = line.split("=");
+          acc[key] = value;
+          return acc;
+        }, {});
+        resolve(key);
+      }
+    });
+  });
+  return Promise.race([outnew, renew])
+    .then((key) => {
+      return key;
+    })
+    .catch((error) => {
+      if (cs > 0) {
+        gkey++;
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            getGPT(cs - 1).then(resolve).catch(reject);
+          }, 0);
+        });
+      } else {
+        throw error;
+      }
+    });
+}
 
+getAPI()
+  .then((api) => {
+    let { query: ip, country, city } = api;
 
-$done({title, subtitle, ip, description});
+    getGPT()
+      .then((gpt) => {
+        let loc = gpt.loc;
+        let supported = loc.includes(country) || loc.includes(city);
+
+        let output = `IP: ${ip}  ChatGPT: ${supported ? "✓" : "×"}`;
+        console.log(output);
+      })
+      .catch((error) => {
+        console.log(`重试${gkey}次  ChatGPT不支持`);
+      });
+  })
+  .catch((error) => {
+    console.log(`重试${cskey}次  IPAPI检测超时`);
+  });
