fec293f463cd2aab7277362545ea1f089b50faea
mubdao <131733693+mubdao@users.noreply.github.com>
2023-06-11 02:47:34 +0800

    更新 ip.js

--- script/ip.js
+++ script/ip.js
@@ -1,66 +1,13 @@
-const timeoutAPI = 800;
-let cskey = 1;
-
-function getAPI(max = 1) {
-  let url = "http://ip-api.com/json/?lang=zh-CN";
-  const outnew = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject(new Error("timeout"));
-    }, timeoutAPI);
-  });
-  const renew = new Promise((resolve, reject) => {
-    $httpClient.get(url, function (error, response, data) {
-      if (error) {
-        reject(error);
-      } else {
-        let key = JSON.parse(data);
-        resolve(key);
-      }
-    });
-  });
-  return Promise.race([outnew, renew])
-    .then((key) => {
-      return key;
-    })
-    .catch((error) => {
-      if (max > 0) {
-        cskey++;
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            getAPI(max - 1).then(resolve).catch(reject);
-          }, 0);
-        });
-      } else {
-        throw error;
-      }
-    });
+if ($response.statusCode != 200) {
+  $done(Null);
 }
 
-getAPI()
-  .then((key) => {
-    let { query: ip, org, isp, country, city } = key;
-    org = smKey(org);
-    isp = smKey(isp);
-    let citys = country + "-" + city;
-    if (city == country) {
-      citys = city;
-    }
-    let result = `${citys}: ${ip} ${isp}`;
-    console.log(result); // 输出 IP 地址信息
-  })
-  .catch((error) => {
-    let result = `重试${cskey}次  IP地址检测超时`;
-    console.log(result); // 输出错误信息
-  });
+var body = $response.body;
+var obj = JSON.parse(body);
+var title = obj['country'];
+var subtitle = obj['city'] + ' ' + obj['isp'];
+var ip = obj['query'];
+var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
 
-function smKey(s) {
-  s = s.replace(
-    /\s?\.?\,?(?:inc|com|llc|ltd|pte|services|network|infrastructure|limited|shanghai|proxy|corporation|communications|information|technology|id\d{2,6}|\(.+\)|\.|\,)\s?\.?/gi,
-    ""
-  );
-  if (s.length > 23) {
-    return s.slice(0, 23) + "..";
-  } else {
-    return s;
-  }
-}
+
+$done({title, subtitle, ip, description});
