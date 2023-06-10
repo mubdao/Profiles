0d619032e9f2502927b4c944738ed2db04400152
mubdao <131733693+mubdao@users.noreply.github.com>
2023-06-11 02:35:15 +0800

    更新 ip.js

--- script/ip.js
+++ script/ip.js
@@ -1,6 +1,5 @@
-const timeoutGPT = 500;
 const timeoutAPI = 800;
-let cskey = 1, gkey = 1, body = {};
+let cskey = 1;
 
 function getAPI(max = 1) {
   let url = "http://ip-api.com/json/?lang=zh-CN";
@@ -37,62 +36,31 @@ function getAPI(max = 1) {
     });
 }
 
-function getGPT(cs = 1) {
-  let url = "http://chat.openai.com/cdn-cgi/trace";
-  const outnew = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject(new Error("timeout"));
-    }, timeoutGPT);
-  });
-  const renew = new Promise((resolve, reject) => {
-    $httpClient.get(url, function (error, response, data) {
-      if (error) {
-        reject(error);
-      } else {
-        let lines = data.split("\n");
-        let key = lines.reduce((acc, line) => {
-          let [key, value] = line.split("=");
-          acc[key] = value;
-          return acc;
-        }, {});
-        resolve(key);
-      }
-    });
-  });
-  return Promise.race([outnew, renew])
-    .then((key) => {
-      return key;
-    })
-    .catch((error) => {
-      if (cs > 0) {
-        gkey++;
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            getGPT(cs - 1).then(resolve).catch(reject);
-          }, 0);
-        });
-      } else {
-        throw error;
-      }
-    });
-}
-
 getAPI()
-  .then((api) => {
-    let { query: ip, country, city } = api;
-
-    getGPT()
-      .then((gpt) => {
-        let loc = gpt.loc;
-        let supported = loc.includes(country) || loc.includes(city);
-
-        let output = `IP: ${ip}  ChatGPT: ${supported ? "✓" : "×"}`;
-        console.log(output);
-      })
-      .catch((error) => {
-        console.log(`重试${gkey}次  ChatGPT不支持`);
-      });
+  .then((key) => {
+    let { query: ip, org, isp, country, city } = key;
+    org = smKey(org);
+    isp = smKey(isp);
+    let citys = country + "-" + city;
+    if (city == country) {
+      citys = city;
+    }
+    let result = `${citys}: ${ip} ${isp}`;
+    console.log(result); // 输出 IP 地址信息
   })
   .catch((error) => {
-    console.log(`重试${cskey}次  IPAPI检测超时`);
+    let result = `重试${cskey}次  IP地址检测超时`;
+    console.log(result); // 输出错误信息
   });
+
+function smKey(s) {
+  s = s.replace(
+    /\s?\.?\,?(?:inc|com|llc|ltd|pte|services|network|infrastructure|limited|shanghai|proxy|corporation|communications|information|technology|id\d{2,6}|\(.+\)|\.|\,)\s?\.?/gi,
+    ""
+  );
+  if (s.length > 23) {
+    return s.slice(0, 23) + "..";
+  } else {
+    return s;
+  }
+}
