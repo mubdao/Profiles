f78bb611fe73ee411532103cdfe4f28373a5f1a0
mubdao <131733693+mubdao@users.noreply.github.com>
2023-06-13 21:28:43 +0800

    Delete myfitnesspal.js

--- script/myfitnesspal.js
+++
@@ -1,9 +0,0 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-
-obj.subscriptionEndDate = "2029-05-01T10:10:53.929Z",
-obj.subscriptionStartDate = "2022-05-01T10:10:53.929Z",
-obj.hasPremium = true,
-
-body = JSON.stringify(obj);
-$done({body}); 
