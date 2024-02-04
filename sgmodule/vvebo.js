01970b5884eb098108ef7eb3723c2614a2270114
mubdao <131733693+mubdao@users.noreply.github.com>
2024-02-04 19:21:35 +0800

    更新 vvebo.js

--- sgmodule/vvebo.js
+++ sgmodule/vvebo.js
@@ -1,9 +1,14 @@
+/******************************
+QuantumultX 修复 vvebo 用户主页的显示脚本
+参考：https://raw.githubusercontent.com/suiyuran/stash/main/override/fix-vvebo.stoverride
+*************************
+
 [mitm]
 hostname = api.weibo.cn
 [rewrite_local]
-^https:\/\/api\.weibo\.cn\/2\/users\/show\? url script-request-header https://raw.githubusercontent.com/bin64/Scripts/main/QuantumultX/vvebo.js
-^https:\/\/api\.weibo\.cn\/2\/statuses\/user_timeline\? url script-request-header https://raw.githubusercontent.com/bin64/Scripts/main/QuantumultX/vvebo.js
-^https:\/\/api\.weibo\.cn\/2\/statuses\/user_timeline\? url script-response-body https://raw.githubusercontent.com/bin64/Scripts/main/QuantumultX/vvebo.js
+^https:\/\/api\.weibo\.cn\/2\/users\/show\? url script-request-header https://raw.githubusercontent.com/mubdao/Profiles/main/sgmodule/vvebo.js
+^https:\/\/api\.weibo\.cn\/2\/statuses\/user_timeline\? url script-request-header https://raw.githubusercontent.com/mubdao/Profiles/main/sgmodule/vvebo.js
+^https:\/\/api\.weibo\.cn\/2\/statuses\/user_timeline\? url script-response-body https://raw.githubusercontent.com/mubdao/Profiles/main/sgmodule/vvebo.js
 *************************
 
 *****************************************/
