7062020bd31ba5ebec88e9cb420ce2c77c58bfaf
mubdao <131733693+mubdao@users.noreply.github.com>
2024-03-30 12:40:55 +0800

    Update Nicegram.js

--- Scripts/Nicegram.js
+++ Scripts/Nicegram.js
@@ -1,35 +1,17 @@
-**************************************
+/*
+
+Nicegram 1.4.7
 
 [rewrite_local]
-^https?:\/\/(nicegram\.cloud\/api\/v\d\/user\/info|restore-access\.indream\.app\/restoreAccess) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/nicegram.js
+https://nicegram.cloud/api/v6/user/info url script-response-body Vvebo-request = type=http-request,pattern=^https:\/\/api\.weibo\.cn\/2\/(users\/show\?|statuses\/user_timeline\?),requires-body=1,script-path=https://raw.githubusercontent.com/mubdao/Profiles/main/Scripts/Nicegram.js
 
-[mitm]
-hostname = nicegram.cloud, restore-access.indream.app
+[mitm] 
+hostname = nicegram.cloud
 
-*************************************/
+**/
 
-
-const url = $request.url;
-const isQX = typeof $task !== "undefined";
-var chxm1023 = JSON.parse($response.body);
-const subscriptionTest = /https:\/\/nicegram\.cloud\/api\/v\d\/user\/info/;
-const premiumTest = /https:\/\/restore-access\.indream\.app\/restoreAccess/;
-
-if (subscriptionTest.test(url)) {
-  chxm1023.data.user = {
-    ...chxm1023.data.user,
-    subscription: true,
-    store_subscription: true,
-    lifetime_subscription: true
-  };
-}
-
-if (premiumTest.test(url)) {
-  chxm1023["data"] = {"premiumAccess": true};
-}
-
-function finalizeResponse(content) {
-  return { status: isQX ? "HTTP/1.1 200 OK" : 200, headers: $response.headers, body: JSON.stringify(content) };
-}
-
-$done(isQX ? finalizeResponse(chxm1023) : chxm1023);
+var Q = JSON.parse($response.body);
+Q.data.user.lifetime_subscription = true;
+Q.data.user.store_subscription = true;
+Q.data.user.subscription = true;
+$done({body : JSON.stringify(Q)});
