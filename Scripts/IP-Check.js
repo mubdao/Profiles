216d6f82829dfded58e6b80e02d37d46f2a7a802
mubdao <131733693+mubdao@users.noreply.github.com>
2025-01-06 15:28:23 +0800

    Update IP-Check.js

--- Scripts/IP-Check.js
+++ Scripts/IP-Check.js
@@ -10,7 +10,9 @@ $httpClient.get(url, function(error, response, data){
     
     body = {
         title: "节点信息",
-        content: `IP信息：${ip}\n运营商：${isp}\n所在地：${emoji}${country} - ${city}`
+        content: `IP：${ip}\nSP：${isp}\nHQ：${country} - ${city}`,    // 不显示国旗emoji
+        // content: `IP：${ip}\nSP：${isp}\nHQ：${emoji}${country} - ${city}`,    // 显示国旗emoji的版本
+        // icon: "globe.asia.australia.fill"    // 图标暂时不显示
     }
     $done(body);
 });
