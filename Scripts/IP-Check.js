5765fbbc3b512545ff6678029cd48bdad9a77e71
mubdao <131733693+mubdao@users.noreply.github.com>
2025-01-06 12:09:32 +0800

    Update IP-Check.js

--- Scripts/IP-Check.js
+++ Scripts/IP-Check.js
@@ -7,22 +7,21 @@ $httpClient.get(url, function(error, response, data){
     let city = jsonData.city
     let isp = jsonData.isp
     let ip = jsonData.query
-  body = {
-    title: "节点信息",
-    content: `IP信息：${ip}\n运营商：${isp}\n所在地：${emoji}${country} - ${city}`,
-    icon: "globe.asia.australia.fill"
-  }
-  $done(body);
+    
+    body = {
+        title: "节点信息",
+        content: `IP信息：${ip}\n运营商：${isp}\n所在地：${emoji}${country} - ${city}`
+    }
+    $done(body);
 });
 
 function getFlagEmoji(countryCode) {
-      if (countryCode.toUpperCase() == 'TW') {
-    countryCode = 'CN'
-  }
-  const codePoints = countryCode
-    .toUpperCase()
-    .split('')
-    .map(char => 127397 + char.charCodeAt())
-  return String.fromCodePoint(...codePoints)
-
+    if (countryCode.toUpperCase() == 'TW') {
+        countryCode = 'CN'
+    }
+    const codePoints = countryCode
+        .toUpperCase()
+        .split('')
+        .map(char => 127397 + char.charCodeAt())
+    return String.fromCodePoint(...codePoints)
 }
