274ffef740428da4b681b28271890a99967e9220
mubdao <131733693+mubdao@users.noreply.github.com>
2025-06-27 22:29:36 +0800

    Update Net.js

--- Scripts/Net.js
+++ Scripts/Net.js
@@ -140,7 +140,7 @@ async function g(e = "/v1/requests/recent", t = "GET", n = null) {
     if (f) content.push(f);
     if (p) content.push(p);
     a = {
-        title: l || "节点信息",
+        title: l || "",
         content: content.join("\n\n")
     };
 })().catch(e => {
