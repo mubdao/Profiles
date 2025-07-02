461e58db03f2327c3c20308054921e3de823939e
mubdao <131733693+mubdao@users.noreply.github.com>
2025-07-02 17:24:11 +0800

    Update Netisp.js

--- Scripts/Netisp.js
+++ Scripts/Netisp.js
@@ -147,7 +147,7 @@ async function g(e = "/v1/requests/recent", t = "GET", n = null) {
     if (f) content.push(f);
     if (p) content.push(p);
     a = {
-        title: l || "NETISP",
+        title: l || "节点信息",
         content: content.join("\n\n")
     };
 })().catch(e => {
