814acd0cedf1c8cc2d40ba32264617bd3cf406be
mubdao <131733693+mubdao@users.noreply.github.com>
2025-07-08 15:00:38 +0800

    Update vvebo.js

--- Module/vvebo/vvebo.js
+++ Module/vvebo/vvebo.js
@@ -1,24 +1,37 @@
-// @suiyuran
 let url = $request.url;
-let hasUid = (url) => url.includes("uid");
-let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);
+let hasUid = (url) => /uid=\d+/.test(url);
+let getUid = (url) => {
+    let match = url.match(/uid=(\d+)/);
+    return match ? match[1] : undefined;
+};
+
 if (url.includes("users/show")) {
-  $persistentStore.write(getUid(url), "uid");
-  $done({});
+    let uid = getUid(url);
+    if (uid) $persistentStore.write(uid, "uid");
+    $done({});
 } else if (url.includes("statuses/user_timeline")) {
-  let uid = getUid(url) || $persistentStore.read("uid");
-  url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
-  url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
-  $done({ url });
+    let uid = getUid(url) || $persistentStore.read("uid");
+    if (!uid) {
+        $done({});
+        return;
+    }
+    url = url.replace("statuses/user_timeline", "profile/statuses/tab")
+             .replace("max_id", "since_id");
+    url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
+    $done({ url });
 } else if (url.includes("profile/statuses/tab")) {
-  let data = JSON.parse($response.body);
-  let statuses = data.cards
-    .map((card) => (card.card_group ? card.card_group : card))
-    .flat()
-    .filter((card) => card.card_type === 9)
-    .map((card) => card.mblog);
-  let sinceId = data.cardlistInfo.since_id;
-  $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
+    try {
+        let data = JSON.parse($response.body);
+        let statuses = data.cards
+            .flatMap(card => card.card_group || [card])
+            .filter(card => card.card_type === 9)
+            .map(card => card.mblog);
+        let sinceId = data.cardlistInfo?.since_id;
+        $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
+    } catch (e) {
+        console.log(`JSON Error: ${e}`);
+        $done({});
+    }
 } else {
-  $done({});
+    $done({});
 }
